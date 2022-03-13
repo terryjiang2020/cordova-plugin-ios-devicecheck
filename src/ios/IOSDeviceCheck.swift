c462d734f2a3ce53d9cabe848f95c6257c463f25
dderick <93905052+dderick@users.noreply.github.com>
2022-03-13 14:26:36 -0700

    complete generateToken

--- src/ios/IOSDeviceCheck.swift
+++ src/ios/IOSDeviceCheck.swift
@@ -2,19 +2,33 @@ import DeviceCheck
 
 
 @objc(IOSDeviceCheck) class IOSDeviceCheck : CDVPlugin {
-    let curDevice = DCDevice.current
 
     // MARK: - REVERSE GEOCODE
     @objc(generateToken:) func generateToken(_ command: CDVInvokedUrlCommand) {
         var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
 
-         if (true) {
-             let result = 'generatedToken'
-             pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: result)
-         }
-         else {
-             pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Expected a non-empty string argument.")
-         }
-        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+        let curDevice = DCDevice.current
+
+        var deviceToken = ""
+
+        if curDevice.isSupported {
+            DCDevice.current.generateToken { (data, error) in
+                if let data = data {
+                    deviceToken = data.base64EncodedString()
+                    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: deviceToken)
+                    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+                }
+
+                if let error = error {
+                    print(error.localizedDescription)
+                    pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "")
+                    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+                }
+            }
+        } else {
+            print("Platform is not supported. Make sure you aren't running in an emulator.")
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "")
+            self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+        }
     }
 }
